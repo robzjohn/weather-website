bdc7d090e2ec1bf59c5d8a694772682a8861cb19
rob <robert.z.j64@gmail.com>
2021-02-06 18:36:59 -0500

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 
 // Define paths for express config
@@ -103,6 +104,7 @@ app.get('*', (req, res) => {
         errorMessage: '404 Error page not found'
     })
 })
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
